cde4ee2a9943d6c73e18395b9e11cef1c5759f05
abdullahquantumx <abdullahquantum754@gmail.com>
2024-12-05 17:58:50 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-4">
+      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+//Changes to made to access Tailwind css along with Vite+React
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
+})
